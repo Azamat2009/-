55c74ce2f8c239af1477cebfea7bf8475c5b18de
Azamat <rakhmatov.azamat2009@gmail.com>
2022-12-04 13:24:42 +0500

    last homework
    homw

---
+++ script.js
@@ -0,0 +1,15 @@
+//2 
+let ch = Math.random()
+let ran = 'sasaas'
+for (var mathrandom of ran) {
+    console.log(ch +  ran )
+}
+
+// 1
+let imya = prompt('Your name')
+let familiya = prompt('Your surname')
+for (let email of imya) {
+    alert(`hi it's your email ${imya}${familiya}${Math.floor(Math.random() * 100) + 1}@gmail.com`)
+}
+    
+
